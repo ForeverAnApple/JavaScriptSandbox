197399dca79c318ba2a4ef3e01e683ace522bcf6
Daiwei Chen <kinbd8@gmail.com>
2018-11-14 23:53:10 -0600

    Added some sample async await code.

---
+++ AsyncAwait/simpleExample.js
@@ -0,0 +1,36 @@
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log('calling');
+  var result = await resolveAfter2Seconds();
+  console.log(result);
+  console.log('after');
+  // expected output: 'resolved'
+}
+
+//asyncCall();
+
+// ---------------------------------------
+
+async function f() {
+
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("done!"), 1000)
+  });
+
+  let result = await promise; // wait till the promise resolves (*)
+
+  console.log(result); // "done!"
+}
+
+f();
+
+// Example: Take out async!
+
+// ---------------------------------------
